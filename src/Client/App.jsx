7d064fe73a8fd86006b1c8e3ebcad180e547a345
LOG67 <m.einakchi@gmail.com>
2017-11-28 18:29:23 -0500

    Fixed the compile and run workflow

--- src/Client/App.jsx
+++ src/Client/App.jsx
@@ -52,21 +52,8 @@ function auth() {
   firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         alert(user.uid)
-        // this.uid = user.uid;
-        // firebase.database().ref(`users/${this.uid}/notes`).on('value', (snap) => {
-        //   const notesObjs = snap.val() || {};
-        //   const notes = _.values(notesObjs);
-        //   this.notes = notes
-        //   .map(n => (n.tags ? n : { ...n, tags: [] }));
-        //   // .map(n => (n.body ? n.body : ''))
-        //   // .map(n => (n.updated_at ? n.updated_at : 0))
-        //   this.subscribers.map(f => f(this.notes));
-        // });
       }
-    });
-
-    // firebase.auth().signInAnonymously()
-
+    })
 }
 
 function init() {
