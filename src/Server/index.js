bf3adde84ab8fd3ecb806dfa369ca5be0ef5b802
LOG67 <m.einakchi@gmail.com>
2017-11-28 19:27:47 -0500

    Added google finance and moment libs, added dist files to gitignore

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,18 +1,22 @@
-const express = require('express')
+import express from 'express'
+import path from 'path'
+import moment from 'moment'
+import googleFinance from 'google-finance'
+
 const app = express()
-const path = require('path')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
-  // res.send('Hello World!')
+})
+
+app.get('/company/:company/startdate/:startdate/enddate/:enddate', (req, res) => {
+  res.send(req.params)
 })
 
 app.get('/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'bundle.js'))
-  // res.send('Hello World!')
 })
-
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
