40709a92d96e612ebc0ce6353cd31d48da0f2eee
rhbrian <brianhrhee@gmail.com>
2017-11-28 21:53:37 -0500

    removed trivial code

--- src/Client/SearchBar.jsx
+++ src/Client/SearchBar.jsx
@@ -36,7 +36,6 @@ export default class SearchBar extends Component {
     }*/
 
     render() {
-        const { submitting } = this.props
 
         return (
             <form onSubmit={e => this.handleSubmit(e)}>
