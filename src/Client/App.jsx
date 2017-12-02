0b96dcf7e7f8560fdb1ff9747198a39e62270348
HanWang <erenwh@gmail.com>
2017-12-02 16:54:11 -0500

    Clean up main render

--- src/Client/App.jsx
+++ src/Client/App.jsx
@@ -17,75 +17,22 @@ class App extends Component {
 
     render() {
         return (
-            <body>
-            <header>
-                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                    <a class="navbar-brand" href="#">Dashboard</a>
-                    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse"
-                            data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Profile</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">History</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
-                            </li>
-                        </ul>
-
-                    </div>
-                </nav>
-            </header>
-
             <div class="container-fluid">
                 <div class="row">
                     <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-                        <ul class="nav nav-pills flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">History 1</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">History 2</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">History 3</a>
-                            </li>
-                        </ul>
+                        <SideBar results={['a', 'b', 'c']}/>
                     </nav>
-
                     <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
-                        <h1>Dashboard</h1>
-
-
+                        <h1>CSA</h1>
+                        <div id="firebaseui-auth-container"></div>
                         <section class="row text-center placeholders">
-                            <SideBar results={['a', 'b', 'c']}/>
                             <SearchBar/>
                             <SearchResults/>
                         </section>
-
-                        <h2>Section title</h2>
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                            </table>
-                        </div>
                     </main>
                 </div>
             </div>
 
-            <div id="firebaseui-auth-container"></div>
-            </body>
 
         )
     }
