f27dddbfb42adba2771db33190fea6cd9cedf81e
HanWang <erenwh@gmail.com>
2017-12-03 02:20:45 -0500

    Update React for searchbar sidebar and cosmatic changes for main page including background

--- src/Client/App.jsx
+++ src/Client/App.jsx
@@ -11,41 +11,131 @@ import SearchResults from './SearchResults.jsx'
 class App extends Component {
 
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            userID: '',
-            query: {},
-            histories: [],
-            result: {}
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      userID: '',
+      query: {},
+      histories: [],
+      result: {}
     }
+  }
 
 
-    componentDidMount() {
-        init()
-        auth()
-    }
+  componentDidMount() {
+    init()
+    auth()
+  }
 
-    update(query) {
-        this.setState({ ...this.state, query: query})
-    }
+  update(query) {
+    this.setState({...this.state, query: query})
+  }
 
-    historySearch(results) {
-        this.setState({ ...this.state, results: results})
-    }
+  historySearch(results) {
+    this.setState({...this.state, results: results})
+  }
 
-    historyClick(index) {
-        this.update();
-    }
+  historyClick(index) {
+    this.update();
+  }
 
 
-    render() {
+  render() {
+    var BackgroundPath = "./images/background_image.jpg"
+    var sectionStyle = {
+      width: "100%",
+      height: "60px",
+      backgroundImage: `url(${BackgroundPath})`
+    };
+
+    var res = [
+      {
+        quotes: [],
+        query: {
+          companySymbol: "SAMG",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "GOOGL",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "APPL",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "AMAZON",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "SAMG",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "GOOGL",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "APPL",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "AMAZON",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "APPL",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+      {
+        quotes: [],
+        query: {
+          companySymbol: "AMAZON",
+          from: "12-21-2015",
+          to: "12-21-2016",
+        },
+      },
+    ]
 
     return (
       <div>
-        <nav className="navbar navbar-expand-md navbar-dark navbar-fixed-top bg-info">
-          <a className="navbar-brand" href="#">CSA</a>
+        <nav className="navbar navbar-expand-md navbar-dark navbar-fixed-top"
+             style={sectionStyle}>
+          <a className="navbar-brand" href="#">Company Sentiment Analyzer</a>
           <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse"
                   data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
                   aria-expanded="false" aria-label="Toggle navigation">
@@ -54,15 +144,6 @@ class App extends Component {
 
           <div className="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Search <span class="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Archive</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
             </ul>
             <form className="form-inline mt-2 mt-md-0">
               <button className="btn btn-dark btn-lg" type="login">Login</button>
@@ -70,93 +151,142 @@ class App extends Component {
           </div>
         </nav>
 
-        <div className="container-fluid">
+        <div className="container-fluid ">
           <div className="row">
-            <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-              <SideBar results={['a', 'b', 'c']}/>
-            </nav>
+            <div className="col-sm-3 col-md-2 bg-dark text-light sidebar">
+              <h5 className="text-center text">History</h5>
+              <SideBar results={res}/>
+            </div>
             <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
               {/*-- <div id="firebaseui-auth-container"></div>*/}
-              <section class="row text-center placeholders">
-                <div className="col">
-                  <div className="row">
-                    <SearchBar
-                      query={this.state.query}
-                      updater={(newQuery) => this.update(newQuery)}/>
-                  </div>
-                  <hr />
-                  <div className="row text-center">
-                    <SearchResults
-                        results={this.state.result}
-                        updater={(oldResults) => this.historySearch(oldResults)}/>
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                    <h1>These are results!!!!!</h1><br />
-                  </div>
-                </div>
-              </section>
+              <SearchBar
+                query={this.state.query}
+                updater={(newQuery) => this.update(newQuery)}/>
+              <hr/>
+              <div className="row">
+                <SearchResults
+                  results={this.state.result}
+                  updater={(oldResults) => this.historySearch(oldResults)}/>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+                <p><h1>These are results!!!!!</h1></p>
+
+
+
+
+
+              </div>
             </main>
           </div>
         </div>
 
       </div>
     )
-    }
+  }
 }
 
 
 function auth() {
 
-    var uiConfig = {
-        signInOptions: [
-            // Leave the lines as is for the providers you want to offer your users.
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        ],
-        callbacks: {
-            signInSuccess: function (currentUser, credential, redirectUrl) {
-                // Do something.
-                // Return type determines whether we continue the redirect automatically
-                // or whether we leave that to developer to handle.
-                return false
-            }
-        }
+  var uiConfig = {
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: function (currentUser, credential, redirectUrl) {
+        // Do something.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return false
+      }
     }
+  }
 
-    // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(firebase.auth())
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig)
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth())
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig)
 
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            alert(user.uid)
-        }
-    })
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      alert(user.uid)
+    }
+  })
 }
 
 function init() {
-    firebase.initializeApp(config)
+  firebase.initializeApp(config)
 }
 
 const root = document.getElementById('app')
 ReactDOM.render(
-    <App/>
-    , root)
+  <App/>
+  , root)
 
 /*
     State: {
--- src/Client/SearchBar.jsx
+++ src/Client/SearchBar.jsx
@@ -1,60 +1,69 @@
-import React, {PropTypes, Component } from 'react'
+import React, {PropTypes, Component} from 'react'
 //import companies from './companies.js'
 
 export default class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {name: props.query.companySymbol, fromDate: props.query.from, toDate: props.query.to};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {name: props.query.companySymbol, fromDate: props.query.from, toDate: props.query.to};
+  }
 
-    handleSubmit(e) {
-        props.updater({symbol: this.state.name, from: this.state.fromDate, to: this.state.toDate})
-        alert(this.state.name)
-        e.preventDefault()
-    }
+  handleSubmit(e) {
+    props.updater({symbol: this.state.name, from: this.state.fromDate, to: this.state.toDate})
+    alert(this.state.name)
+    e.preventDefault()
+  }
 
-    handleChangeName(e) {
-        this.setState({name: e.target.value})
-        e.preventDefault()
-    }
+  handleChangeName(e) {
+    this.setState({name: e.target.value})
+    e.preventDefault()
+  }
 
-    handleChangeFromDate(e) {
-        this.setState({fromDate: e.target.value})
-        e.preventDefault()
-    }
+  handleChangeFromDate(e) {
+    this.setState({fromDate: e.target.value})
+    e.preventDefault()
+  }
 
-    handleChangeToDate(e) {
-        this.setState({toDate: e.target.value})
-        e.preventDefault()
-    }
+  handleChangeToDate(e) {
+    this.setState({toDate: e.target.value})
+    e.preventDefault()
+  }
 
 
-    /*() => expression
+  /*() => expression
 
-    function () {
-        return expression
-    }*/
+  function () {
+      return expression
+  }*/
 
-    render() {
-        this.props.updater();
-        return (
-            <form onSubmit={e => this.handleSubmit(e)}>
-                <input type="text"
-                       value={this.state.name}
-                       onChange={e => this.handleChangeName(e)}
-                       placeholder='Company Name' />
-                <input type="text"
-                       value={this.state.fromDate}
-                       onChange={e => this.handleChangeFromDate(e)}
-                       placeholder='From Date: MM/DD' />
-                <input type="text"
-                       value={this.state.toDate}
-                       onChange={e => this.handleChangeToDate(e)}
-                       placeholder='To Date: MM/DD' />
-                <input type="submit"
-                       value="Search"/>
-
-            </form>
-        )
-    }
+  render() {
+    this.props.updater();
+    return (
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <div class="form-row">
+          <div class="col-4">
+            <input class="form-control "
+                   type="text"
+                   value={this.state.name}
+                   onChange={e => this.handleChangeName(e)}
+                   placeholder='Company Name'/>
+          </div>
+          <div class="col">
+            <input class="form-control "
+                   type="text"
+                   value={this.state.fromDate}
+                   onChange={e => this.handleChangeFromDate(e)}
+                   placeholder='From MM/DD/YYYY'/>
+          </div>
+          <div class="col">
+            <input class="form-control "
+                   type="text"
+                   value={this.state.toDate}
+                   onChange={e => this.handleChangeToDate(e)}
+                   placeholder='To MM/DD/YYYY'/>
+          </div>
+          <button class="btn btn-dark text-center" type="submit">Search</button>
+        </div>
+      </form>
+    )
+  }
 }
