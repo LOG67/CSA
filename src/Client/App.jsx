72fdc78431a7eef61cab03d0eefa9f91dca2dda5
HanWang <erenwh@gmail.com>
2017-12-02 21:18:07 -0500

    merge conflex

--- src/Client/App.jsx
+++ src/Client/App.jsx
@@ -22,63 +22,6 @@ class App extends Component {
     }
 
 
-    componentDidMount() {
-        init()
-        auth()
-    }
-
-
-    render() {
-
-        return (
-            <div>
-                <nav className="navbar navbar-expand-md navbar-dark navbar-fixed-top bg-dark">
-                    <a className="navbar-brand" href="#">CSA</a>
-                    <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse"
-                        data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Search <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Archive</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
-                            </li>
-                        </ul>
-                        <form className="form-inline mt-2 mt-md-0">
-                            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"></input>
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                    </div>
-                </nav>
-
-                <div className="container-fluid">
-                    <div className="row">
-                        <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-                            <SideBar results={['a', 'b', 'c']}/>
-                        </nav>
-                        <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
-                            {/*-- <div id="firebaseui-auth-container"></div>*/}
-                            <section class="row text-center placeholders">
-                                <SearchBar/>
-                                <SearchResults/>
-                            </section>
-                        </main>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-  }
-
-
   componentDidMount() {
     init()
     auth()
