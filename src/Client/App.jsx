4fa65d0ad7b19a1619354c7def0b7f0d9ee89f8f
HanWang <erenwh@gmail.com>
2017-12-02 19:05:35 -0500

    Modify sidebar, improved react

--- src/Client/App.jsx
+++ src/Client/App.jsx
@@ -10,71 +10,100 @@ import SearchResults from './SearchResults.jsx'
 
 class App extends Component {
 
-    componentDidMount() {
-        init()
-        auth()
-    }
 
-    render() {
 
-        return (
-            <body>
-            <header>
-                <h1>CSA</h1>
-            </header>
-            <div class="container-fluid">
-                <div class="row">
-                    <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-                        <SideBar results={['a', 'b', 'c']}/>
-                    </nav>
-                    <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
-                        {/*-- <div id="firebaseui-auth-container"></div>*/}
-                        <section class="row text-center placeholders">
-                            <SearchBar/>
-                            <SearchResults/>
-                        </section>
-                    </main>
-                </div>
-            </div>
-            </body>
+  componentDidMount() {
+    init()
+    auth()
+  }
 
-        )
-    }
+
+  render() {
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark navbar-fixed-top bg-dark">
+          <a className="navbar-brand" href="#">CSA</a>
+          <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse"
+                  data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
+                  aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Search <span class="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Archive</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+            </ul>
+            <form className="form-inline mt-2 mt-md-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"></input>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+
+        <div className="container-fluid">
+          <div className="row">
+            <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+              <SideBar results={['a', 'b', 'c']}/>
+            </nav>
+            <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
+              {/*-- <div id="firebaseui-auth-container"></div>*/}
+              <section class="row text-center placeholders">
+                <SearchBar/>
+                <SearchResults/>
+              </section>
+            </main>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 function auth() {
 
-    var uiConfig = {
-        signInOptions: [
-            // Leave the lines as is for the providers you want to offer your users.
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        ],
-        callbacks: {
-            signInSuccess: function (currentUser, credential, redirectUrl) {
-                // Do something.
-                // Return type determines whether we continue the redirect automatically
-                // or whether we leave that to developer to handle.
-                return false
-            }
-        }
+  var uiConfig = {
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: function (currentUser, credential, redirectUrl) {
+        // Do something.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return false
+      }
     }
+  }
 
-    // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(firebase.auth())
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig)
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth())
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig)
 
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            alert(user.uid)
-        }
-    })
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      alert(user.uid)
+    }
+  })
 }
 
 function init() {
-    firebase.initializeApp(config)
+  firebase.initializeApp(config)
 }
 
 const root = document.getElementById('app')
-ReactDOM.render(<App/>, root)
+ReactDOM.render(
+  <App/>
+  , root)
--- src/Client/SideBar.jsx
+++ src/Client/SideBar.jsx
@@ -15,10 +15,10 @@ export default class SideBar extends Component {
         for (var i = 0; i < history.length; i++) {
             // note: we add a key prop here to allow react to uniquely identify each
             // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-            rows.push(<li key={i}> <a> {history[i]}</a></li>);
+            rows.push(<li key={i}> <a href={"#"}> {history[i]}</a></li>);
         }
         return (
-                <ul>
+                <ul className="list-unstyled text-center">
                     {rows}
                 </ul>
         )
