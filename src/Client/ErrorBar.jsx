7a972be0ca56bcd4efd2bbe2aab275452eb69ce4
Han Wang <erenwh@gmail.com>
2017-12-04 05:11:26 -0500

    center error bar

--- src/Client/ErrorBar.jsx
+++ src/Client/ErrorBar.jsx
@@ -12,16 +12,14 @@ export default class ErrorBar extends Component {
         }
         return (
             <div
-                className="mx-auto"
+                className="text-center"
                 id="errorEleID"
                 style={showError ? {visibility: 'visible'} : {display: 'none'}}>
                 {res.map(i => {
                     return <div>{i}</div>
                 })}
                 <button
-                    className="btn btn-dark"
-                    id='errorBtn'
-                    onClick={console.log("test")}>
+                    className="btn btn-dark">
                     Dismiss
                 </button>
 
