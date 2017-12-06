fd5cf9c6c51bc6955493d4a29394894c2748f36d
HanWang <erenwh@gmail.com>
2017-12-06 00:34:00 -0500

    Centralize sign-in page

--- src/Client/App.jsx
+++ src/Client/App.jsx
@@ -1,100 +1,119 @@
-    import React, {Component} from 'react'
-    import ReactDOM from 'react-dom'
-    import * as firebase from 'firebase'
-    import * as firebaseui from 'firebaseui'
-    import axios from 'axios'
-    import _ from 'lodash'
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import * as firebase from 'firebase'
+import * as firebaseui from 'firebaseui'
+import axios from 'axios'
+import _ from 'lodash'
 
-    import config from '../../config.js'
-    import SearchBar from './SearchBar.jsx'
-    import SideBar from './SideBar.jsx'
-    import SearchResult from './SearchResult.jsx'
-    import NavBar from './NavBar.jsx'
-    import ErrorBar from "./ErrorBar.jsx"
+import config from '../../config.js'
+import SearchBar from './SearchBar.jsx'
+import SideBar from './SideBar.jsx'
+import SearchResult from './SearchResult.jsx'
+import NavBar from './NavBar.jsx'
+import ErrorBar from "./ErrorBar.jsx"
 
-    import dummyData from './DummyData.json'
+import dummyData from './DummyData.json'
 
-    const SERVER_URL = 'http://localhost:3000/'
+const SERVER_URL = 'http://localhost:3000/'
 
-    class App extends Component {
-        constructor(props) {
-            super(props)
-            this.state = {
-                userID: '',
-                username: '',
-                query: {},
-                histories: [],
-                result: {},
-                errors: [],
-            }
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            userID: '',
+            username: '',
+            query: {},
+            histories: [],
+            result: {},
+            errors: [],
+        }
+    }
+
+
+    componentDidMount() {
+        init(this)
+        auth()
+    }
+
+
+    // Event Handlers
+
+    onLogoutPressed() {
+        firebase.auth().signOut().then(() => {
+            this.setState({...this.state, username: ""})
+
+        }).catch(error => {
+            console.log("logout error")
+        })
+        auth()
+    }
+
+    onQueryChanged(query) {
+        this.setState({ ...this.state, query})
+    }
+
+    onSubmitPressed(errors) {
+        console.log("errors: " + errors)
+        this.setState({ ...this.state, errors })
+        if (errors.length > 0) {
+            return
         }
 
+        firebase.auth().currentUser.getIdToken(true).then(idToken => {
+            let url = SERVER_URL + 'query/symbol/' + this.state.query.companySymbol + '/from/' +
+            this.state.query.from + '/to/' + this.state.query.to + '/token/' + idToken
+            return axios.get(url)
+        }).then(res => {
+            this.setState({...this.state, result: res.data})
+        }).catch(function(error) {
+            console.log(error)
+        })
+    }
 
-        componentDidMount() {
-            init(this)
-            auth()
+    onHistorySelected(index) {
+        const result = this.state.histories[index]
+        const query = result.query
+        this.setState({ ...this.state, result, query })
+    }
+
+    onErrorDismissed() {
+        this.setState({...this.state, errors:[]})
+
+    }
+    /* objectSpread*/
+
+
+    render() {
+        var signedIn = false;
+        if (this.state.username.length === 0) {
+            signedIn = false;
+        } else {
+            signedIn = true;
         }
-
-
-        // Event Handlers
-
-        onLogoutPressed() {
-            firebase.auth().signOut().then(() => {
-                this.setState({...this.state, username: ""})
-
-            }).catch(error => {
-                console.log("logout error")
-            })
-            auth()
-        }
-
-        onQueryChanged(query) {
-            this.setState({ ...this.state, query})
-        }
-
-        onSubmitPressed(errors) {
-            console.log("errors: " + errors)
-            this.setState({ ...this.state, errors })
-            if (errors.length > 0) {
-                return
-            }
-
-            firebase.auth().currentUser.getIdToken(true).then(idToken => {
-                let url = SERVER_URL + 'query/symbol/' + this.state.query.companySymbol + '/from/' +
-                    this.state.query.from + '/to/' + this.state.query.to + '/token/' + idToken
-                return axios.get(url)
-            }).then(res => {
-                this.setState({...this.state, result: res.data})
-            }).catch(function(error) {
-                console.log(error)
-            })
-        }
-
-        onHistorySelected(index) {
-            const result = this.state.histories[index]
-            const query = result.query
-            this.setState({ ...this.state, result, query })
-        }
-
-        onErrorDismissed() {
-            this.setState({...this.state, errors:[]})
-
-        }
-        /* objectSpread*/
-
-
-        render() {
-            return (
+        return (
+            <div>
+                <NavBar
+                    username={this.state.username}
+                    onLogoutPressed={() => this.onLogoutPressed()}
+                />
                 <div>
-                    <NavBar
-                        username={this.state.username}
-                        onLogoutPressed={() => this.onLogoutPressed()}
-                    />
-                    <div>
-                        <ErrorBar  errors={this.state.errors}
+                    <ErrorBar  errors={this.state.errors}
                         dismiss={() => this.onErrorDismissed()}/>
                     </div>
-                    <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-4"></div>
+                        <div className="col-md-4">
+                            <div
+                                style={signedIn ? {display: 'none'} :
+                                {visibility: 'visible'}}
+                                id="firebaseui-auth-container">
+                            </div>
+                        </div>
+                        <div className="col-md-4"></div>
+                    </div>
+                    <div className="container-fluid"
+                        style={signedIn ? {visibility: 'visible'} :
+                        {display: 'none'}}>
                         <div className="row">
                             <div className="col col-md-2">
                                 <div className=" sidebar-light sidebar mt-md-3"
@@ -108,11 +127,11 @@
                             </div>
                             <div className="col col-sm-9 ml-sm-auto col-md-10 bg-light text-dark">
                                 <div className=" mt-md-3">
-                                <SearchBar
-                                    query={this.state.query}
-                                    onSubmitPressed={(errors) => this.onSubmitPressed(errors)}
-                                    onQueryChanged={newQuery => this.onQueryChanged(newQuery)}
-                                />
+                                    <SearchBar
+                                        query={this.state.query}
+                                        onSubmitPressed={(errors) => this.onSubmitPressed(errors)}
+                                        onQueryChanged={newQuery => this.onQueryChanged(newQuery)}
+                                    />
                                 </div>
                                 <hr/>
                                 <div className="row">
@@ -120,7 +139,7 @@
                                         result={this.state.result}
                                     />
                                 </div>
-                                <div id="firebaseui-auth-container"></div>
+
                             </div>
                         </div>
                     </div>
@@ -184,29 +203,29 @@
     query: t.Query,
     result: t.Result,
     histories: [t.Result],
-    }
+}
 
-    Result: {
-    query: t.Query,
-    quotes: [t.Quote],
-    tone: t.Tone,
-    date: t.String,
-    }
+Result: {
+query: t.Query,
+quotes: [t.Quote],
+tone: t.Tone,
+date: t.String,
+}
 
-    Query: {
-    companySymbol: t.String,
-    startDate: t.String,
-    endDate: t.String,
-    }
+Query: {
+companySymbol: t.String,
+startDate: t.String,
+endDate: t.String,
+}
 
-    Tone: {
-    ??
-    }
+Tone: {
+??
+}
 
-    Quote: {
-    qDate: t.String,
-    open: t.Number,
-    close: t.Number,
-    volume: t.Number,
-    }
-    */
+Quote: {
+qDate: t.String,
+open: t.Number,
+close: t.Number,
+volume: t.Number,
+}
+*/
